96ed2abff5b9195962e32d206ba9863cfa03fe81
Thanos <atha.ar@arcor.de>
2019-11-17 23:07:53 +0100

    update()

--- config.py
+++ config.py
@@ -1,11 +1,24 @@
-cfg_d = '''{   
-                \n\t\"username\": \"\",
-                \n\t\"userid\": \"\",
-                \n\t\"password\": \"\",
-                \n\t\"pin\": \"\",
-                \n\t\"secret\": \"\",
-                \n\t\"sampletext\": \"\",
-                \n\t\"sampletext\": \"\",
-                \n\t\"sampletext\": \"\",
-                \n\t\"sampletext\":
-            }'''
+import json, os, sys
+
+def read():
+
+    if(os.path.isfile("config.json")):
+        config = open("config.json", "r")
+        cfg = json.loads(config.read())
+    else:
+        from default_config import cfg_d
+        _config = open("config.json", "w")
+        json.dump(cfg_d, _config) 
+        _config.close()
+        print("Created default config.json. Configure and then run again.")
+        sys.exit()
+        
+    return cfg
+
+def update(keys, values):
+    cfg = read()
+    for i in range(keys.lenght):
+        cfg[keys[i]] = values[i]
+    _config = open("config.json", "w")
+    json.dump(cfg, _config) 
+    _config.close()
